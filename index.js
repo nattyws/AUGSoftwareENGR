ea2af1b08a6e181e2ab229c68537e69b694c05f0
MohamedAli9 <69191635+MohamedAli9@users.noreply.github.com>
2023-11-01 22:18:39 -0500

    This is my changes on the dropdown menu

--- index.js
+++ index.js
@@ -12,5 +12,40 @@ document.addEventListener("DOMContentLoaded", function() {
     slider.oninput = function() {
       output.innerHTML = this.value;
     }
+
+
+    //  Mohamed Ali's changes
+    var dropdownMenu = document.getElementById("dropdown");
+    dropdownMenu.addEventListener ('change', () => {
+      console.log(dropdownMenu.options[dropdownMenu.selectedIndex].value);
+
+      var selectedOption = dropdownMenu.options[dropdownMenu.selectedIndex].value;
+       var memorable = document.getElementById("memorableDiv");
+       var random = document.getElementById("randomDiv");
+      if (selectedOption === 'random') {
+        memorable.style.display = 'none';
+        random.style.display = 'inline';
+
+      }
+      else if (selectedOption === 'memorable') {
+        random.style.display = 'none';
+        memorable.style.display = 'inline';
+
+      } else {
+        random.style.display = 'none';
+        memorable.style.display = 'none';
+
+      }
+    })
+
+
+
   });
+
+
+
+
+
+
+
   
